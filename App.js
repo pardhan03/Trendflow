55e3ab3df4ac078a6febda4e6580abd6014af1e6
Manish-Intern <pardhanmanish7887@gmail.com>
2025-01-05 17:57:30 +0530

    project setup

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+
+
+export default App;
